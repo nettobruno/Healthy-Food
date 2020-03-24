ff92805d1cf4f1d2b639679786161219ab12dc9d
Bruno Netto <brunonettomac@outlook.com>
2020-03-24 18:02:44 -0300

    Single image slide to width up to 767 px

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 // Efeito de slide nas imagens do blog
-$('.slide').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
-});
+  $('.slide').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
